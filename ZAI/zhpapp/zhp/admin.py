db928cd046e42e2960b464c5505a187c3ee19dd4
bartiriri <bartis312123@gmail.com>
2022-06-10 00:05:31 +0200

    Delete ZAI directory

--- ZAI/zhpapp/zhp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Harcerz, Druzyna, Harcerz_w_druzynie, Stopien, Stopien_harcerza
-# Register your models here.
-admin.site.register(Harcerz)
-admin.site.register(Druzyna)
-admin.site.register(Harcerz_w_druzynie)
-admin.site.register(Stopien)
-admin.site.register(Stopien_harcerza)
